b65456dedb7ab1f17b066a23e87eeeecd43534e6
JesusS-sys <jimenezcamelojesussantiago@gmail.com>
2025-05-25 17:35:16 -0500

    se configura los requirements

--- asistencia/models.py
+++ asistencia/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    
+    usuario_id = models.CharField(
+        verbose_name='Usuario',
+        max_length=150,
+    )
+    acudiente_id = models.CharField(
+        verbose_name='Acudiente',
+        max_length=150,
+    )
+    id_curso = models.PositiveSmallIntegerField(
+        verbose_name='Curso',
+    )
+
+
+class Curso(models.Model) :
+    
+    nombre_curso=models.CharField(
+        verbose_name='Nombre curso',
+        max_length=150,
+    )
+    id_profesor = models.CharField(
+        verbose_name='Profesor',
+        max_length=100,
+    )
--- note_db/settings.py
+++ note_db/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'note_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'note_db',
+        'USER': 'postgres', 
+        'PASSWORD': '321', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

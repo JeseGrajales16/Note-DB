eecdaf88d98b2a2c82b28d60aae271fab1a09f22
StickMora <jhojansmb@gmail.com>
2025-08-14 17:56:28 -0500

    se ajustan views de eliminar y listar

--- asistencia/urls.py
+++ asistencia/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegistroUsuarioView
+from .views import RegistroUsuarioView, ListarUsuariosView, EliminarUsuarioView
 
-urlpatterns =[
-    path("registro/", RegistroUsuarioView.as_view(),name="registro"),
+urlpatterns = [
+    path("registro/", RegistroUsuarioView.as_view(), name="registro"),
+    path("usuarios/", ListarUsuariosView.as_view(), name="usuarios"),
+    path("usuarios/eliminar/<int:pk>/", EliminarUsuarioView.as_view(), name="eliminar_usuario"),
 ]
--- asistencia/views.py
+++ asistencia/views.py
@@ -1,16 +1,29 @@
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import User
 from .forms import UsuarioRegistroForm
+
 
 class RegistroUsuarioView(CreateView):
     model = User
     form_class = UsuarioRegistroForm
     template_name = "registro.html"
-    success_url = reverse_lazy("registro")
+    success_url = reverse_lazy("usuarios")
 
     def form_valid(self, form):
         user = form.save(commit=False)
         user.set_password(form.cleaned_data["password"])
         user.save()
         return super().form_valid(form)
+
+
+class ListarUsuariosView(ListView):
+    model = User
+    template_name = "usuarios.html"
+    context_object_name = "usuarios"
+
+
+class EliminarUsuarioView(DeleteView):
+    model = User
+    template_name = "eliminar_usuario.html"
+    success_url = reverse_lazy("usuarios")
